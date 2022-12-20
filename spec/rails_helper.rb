f0cfca52226b1ec86e5d470fbbc1a0904c3bd2a9
rafael <rafaelsantossgaldino2@gmail.com>
2022-12-20 13:38:33 -0300

    config simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'vcr'
+require 'simplecov'
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
@@ -34,3 +35,5 @@ VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :webmock
 end
+
+SimpleCov.start
